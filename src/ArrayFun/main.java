7cfc71ba0c150f6f0f7105f05b9a45f654aa1524
compsci <compsci@compsci>
2017-11-09 09:29:25 -0700

    commit 2

--- src/ArrayFun/main.java
+++ src/ArrayFun/main.java
@@ -2,4 +2,14 @@ package ArrayFun;
 
 public class main {
 
+public static void main(String[] args) {
+		
+		arrayFun learnArrays = new arrayFun();
+		
+		learnArrays.showInstructions();
+		learnArrays.intArrayExample();
+
+	}
+
+	
 }
